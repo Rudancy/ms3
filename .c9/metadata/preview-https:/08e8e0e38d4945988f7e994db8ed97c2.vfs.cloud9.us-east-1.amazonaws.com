5ff392fb593b8d3a2417dd607243d6b4146b0033
Ubuntu <ubuntu@ip-172-31-34-181.ec2.internal>
2019-11-26 22:48:12 +0000

    added bootstrap theme, bootstrap added initial styles to blog page, created template inheritance

---
+++ .c9/metadata/preview-https:/08e8e0e38d4945988f7e994db8ed97c2.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://08e8e0e38d4945988f7e994db8ed97c2.vfs.cloud9.us-east-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://08e8e0e38d4945988f7e994db8ed97c2.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser"},"timestamp":1574806129417}
--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, session
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'vote_base'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tpa55@mycluster-qbgul.mongodb.net/vote_base?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return "hello"
+@app.route('/homepage')
+def homepage():
+    return render_template("homepage.html")
+
+
+
+@app.route('/user_blogs')
+def get_blogs_page():
+    return render_template("blogs.html", blogs=mongo.db.user_profile.find())
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
