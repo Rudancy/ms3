6e85103c1f0383ef905755c786367634aedaaebe
Ubuntu <ubuntu@ip-172-31-34-181.ec2.internal>
2019-12-20 10:40:35 +0000

    enviroment var setup, styling complete

--- app.py
+++ app.py
@@ -4,41 +4,65 @@ from flask_pymongo import PyMongo
 import datetime
 from flask_wtf import FlaskForm
 from bson.objectid import ObjectId 
-
+from os import path
+if path.exists("env.py"):
+  import env
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = 'the secret'
+ 
+app.config ["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+app.config ["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["MONGO_DBNAME"] = 'vote_base'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tpa55@mycluster-qbgul.mongodb.net/vote_base?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+""""
 
+intial view of project
 
+"""
 @app.route('/')
 @app.route('/home')
 def home():
+
+    
+    
     return render_template('home.html')
 
     
+
+
 @app.route('/user_blogs')
 def user_blogs():
     
+    #read; allows the user to read the blog
+    
     return render_template("user_blogs.html", blogs=mongo.db.user_profile.find())
+
+
 
 @app.route('/add_blog', methods=['GET', 'POST'])
 def add_blog():
+    
+    
+    #finds current information in db and renders in html form
+    
         
         wages=mongo.db.wages.find()
         regions=mongo.db.regions.find()
         parties=mongo.db.parties.find()
         religions=mongo.db.religion.find()
         ages=mongo.db.age_groups.find()
-        return render_template("add_blog.html", ages=ages, regions=regions, wages=wages, parties=parties, religions=religions)
+        comment=mongo.db.user_comment.find()
+        
+        return render_template("add_blog.html", comment=comment, ages=ages, regions=regions, wages=wages, parties=parties, religions=religions)
     
+
 
 @app.route('/insert_blog', methods=['GET', 'POST'])
 def insert_blog():
+    
+    #creates user_blog into DB
     
     now = datetime.datetime.now()
     
@@ -66,20 +90,26 @@ def insert_blog():
 @app.route('/edit_blog/<user_profile_id>')
 def edit_blog(user_profile_id):
     
+    #finds current information of user in db and renders in html form
+    
     user = mongo.db.user_profile.find_one({"_id": ObjectId(user_profile_id)})
     all_blogs=mongo.db.user_profile.find()
     wages=mongo.db.wages.find()
     regions=mongo.db.regions.find()
     parties=mongo.db.parties.find()
     religions=mongo.db.religion.find()
-    ages=mongo.db.age_groups.find()   
+    ages=mongo.db.age_groups.find()
+    comment=mongo.db.user_comment.find()
    
     
     
-    return render_template('edit_blog.html', user=user, blog=all_blogs, ages=ages, regions=regions, wages=wages, parties=parties, religions=religions, user_profile_id=user_profile_id)
+    return render_template('edit_blog.html',comment=comment, user=user, blog=all_blogs, ages=ages, regions=regions, wages=wages, parties=parties, religions=religions, user_profile_id=user_profile_id)
 
 @app.route('/update_blog/<user_profile_id>', methods=['POST'])
 def update_blog(user_profile_id):
+    
+    #updates new information
+    
     user_profile = mongo.db.user_profile
     user_profile.update_one( {'_id': ObjectId(user_profile_id)},
     {'$set':
@@ -98,6 +128,9 @@ def update_blog(user_profile_id):
 
 @app.route('/delete_blog/<user_profile_id>')
 def delete_blog(user_profile_id):
+    
+    #delete function
+    
     mongo.db.user_profile.remove({'_id': ObjectId(user_profile_id)})
     return redirect(url_for('user_blogs'))
     
